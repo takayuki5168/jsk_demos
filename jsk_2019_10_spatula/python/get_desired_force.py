c0a35aac0d08080652e833c19c08d5ac37642e5b
Anne Koepken <anne@koepken.de>
2020-01-21 17:33:10 +0900

    added ts

--- jsk_2019_10_spatula/python/get_desired_force.py
+++ jsk_2019_10_spatula/python/get_desired_force.py
@@ -3,11 +3,14 @@ import rosbag
 import os
 import numpy as np
 
-path_bag = "/home/leus/force_different_spatula_pos/test"
-path_json = "%s/force.json" % path_bag
+
+#path_bag = "/home/leus/force_different_spatula_pos/test"
+path = "/home/leus/force_feedack_exp_19_01"
+path_json = "%s/force.json" % path
 
 data = {}
 force_dict = {}
+ts_dict = {}
 indices = {}
 n_t_dict = {}
 labels = []
@@ -22,8 +25,14 @@ def read_bag():
     force = {}
     force["larm"] = []
     force["rarm"] = []
-    for label in ["long","longer","shorter"]:
-        path_bag = "/home/leus/force_different_spatula_pos/transfer/%s" % label
+    n_exp = 0
+    for label in ["short","long","long_adapted","short_2","short_3","long_2","short_4"]:
+        path_bag = "%s/%s" % (path,label)
+        n_exp = n_exp + len(os.listdir(path_bag))
+
+    for label in ["short","long","long_adapted","short_2","short_3","long_2","short_4"]:
+        #path_bag = "/home/leus/force_different_spatula_pos/transfer/%s" % label
+        path_bag = "%s/%s" % (path,label)
 
         for doc in os.listdir(path_bag):
             if doc.split(".")[-1] != "bag":
@@ -42,18 +51,19 @@ def read_bag():
                 if topic == "/endeffector_force":
                     force["larm"].append(msg.larm)
                     force["rarm"].append(msg.rarm)
-                    ts.append(t)
+                    ts.append(t.to_sec())
 
                 if topic == "/semantic_annotation":
                     [action,flag_type] = msg.data.split("_")
                     if flag_type == "start":
-                        start_ts[action] = t
+                        start_ts[action] = t.to_sec()
+                        print action
                     if flag_type == "end":
-                        stop_ts[action] = t
+                        stop_ts[action] = t.to_sec()
 
-            split_sequence(ts,force,start_ts,stop_ts)
+            split_sequence(ts,force,start_ts,stop_ts,n_exp)
 
-def split_sequence(ts,force,start_ts,stop_ts):
+def split_sequence(ts,force,start_ts,stop_ts,n_exp):
     force_action = {}
     force_action["larm"] = []
     force_action["rarm"] = []
@@ -61,30 +71,45 @@ def split_sequence(ts,force,start_ts,stop_ts):
         if action not in stop_ts.keys():
             continue
         ts = np.array(ts)
+        print "ts shape array"
+        print np.shape(ts)
         start_index = np.argmin(np.abs(ts-start_ts[action]))
         stop_index = np.argmin(np.abs(ts-stop_ts[action]))
+        ts_list_action = ts[start_index:stop_index] - ts[start_index]
         force_action["larm"] = force["larm"][start_index:stop_index]
         force_action["rarm"] = force["rarm"][start_index:stop_index]
 
         if action in force_dict.keys():
-            update_force_dict(action,force_action)
+            update_force_dict(action,force_action,ts_list_action)
         else:
             n_t = np.shape(force_action["larm"])[0]
-            force_array = np.zeros([int(round(1.5*n_t)) ,np.shape(force_action["larm"])[1],20])
+            force_array = np.zeros([int(round(1.5*n_t)) ,np.shape(force_action["larm"])[1],n_exp])
+            ts_array = np.zeros([int(round(1.5*n_t)),n_exp])
+            print "-------------"
+            print np.shape(ts_array)
             force_dict[action] = {}
             force_dict[action]["larm"] = force_array
             force_dict[action]["rarm"] = force_array
             indices[action] = 0
             n_t_dict[action] = []
-            update_force_dict(action,force_action)                      
+            ts_dict[action] = ts_array
+            update_force_dict(action,force_action,ts_list_action)                      
 
-def update_force_dict(action,force):
+def update_force_dict(action,force,ts):
     for arm in ["larm","rarm"]:
         force_array = np.array(force_dict[action][arm])
         n_t = np.shape(force[arm])[0]
         force_array[0:n_t,0:np.shape(force[arm])[1],indices[action]] = force[arm]
         force_dict[action][arm] = force_array.tolist()
-
+    print "ts shaope list"
+    print np.shape(ts)
+    ts_array = np.array(ts_dict[action])
+    print ",,,,,,,,,,,,,,,,,,,"
+    print np.shape(ts_array)
+    ts_array[0:n_t,indices[action]] = ts
+    ts_dict[action] = ts_array.tolist()
+    print "ts shape dict"
+    print np.shape(ts_dict[action])
     n_t_dict[action].append(n_t)
     indices[action] = indices[action] + 1
 
@@ -93,6 +118,7 @@ def save_json():
     data["n_exp"] = indices
     data["label"] = labels
     data["n_t"] = n_t_dict
+    data["ts"] = ts_dict
     json_dict = json.dumps(data)
     f = open(path_json,"w")
     f.write(json_dict)
